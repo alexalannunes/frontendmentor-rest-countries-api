84c4621763840e2f89728501101e7078d9fb40af
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-07-01 15:26:29 -0300

    refactor: config files

---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-size: 16px;
+    font-family: 'Nunito Sans', sans-serif;
+  }
+`;
