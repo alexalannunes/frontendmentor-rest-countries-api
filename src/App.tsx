8a4394cc86682c538cb06983ccc6d6be5357c303
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-07-01 21:10:12 -0300

    chore: setup initial router

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      <span>dkdkd</span>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/app/router.tsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="bg-red-300">Hello world!</div>,
+  },
+]);
