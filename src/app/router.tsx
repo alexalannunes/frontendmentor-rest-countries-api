48e0947917e1a7706a83e4f81dff7c246d3cf2ef
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-07-06 20:30:42 -0300

    feat(route): add country detail page

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "../pages/layout";
 import { CountriesPage } from "../pages/countries/countries";
+import { CountryPage } from "../pages/country/country";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <CountriesPage />,
+      },
+      {
+        path: ":alpha2Code",
+        element: <CountryPage />,
       },
     ],
   },
