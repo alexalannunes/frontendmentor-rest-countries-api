1043d3ba5e86dfeff3fb3d60a0ffe5033c22cad8
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-07-07 18:07:13 -0300

    chore(types): add new props

--- src/types/country.type.ts
+++ src/types/country.type.ts
@@ -1,12 +1,28 @@
 export interface Country {
   name: {
     common: string;
+    nativeName?: {
+      ara?: {
+        common?: string;
+      };
+    };
   };
+  tld?: string[];
+  currencies?: Record<
+    string,
+    | {
+        name?: string;
+        symbol?: string;
+      }
+    | undefined
+  >;
   cca2: string;
   ccn3?: string;
   cca3?: string;
   capital?: string[];
   region: string;
+  languages?: Record<string, string | undefined>;
+  subRegion?: string;
   population: number;
   flags: {
     png: string;
