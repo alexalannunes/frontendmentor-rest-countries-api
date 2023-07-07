aaff29c050ebe2fcf9349d39b395ca2a84b28d98
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-07-06 21:05:47 -0300

    chore(types): remove fields

--- src/types/country.type.ts
+++ src/types/country.type.ts
@@ -5,12 +5,8 @@ export interface Country {
   cca2: string;
   ccn3: string;
   cca3: string;
-  independent: boolean;
-  status: string;
-  unMember: boolean;
   capital: string[];
   region: string;
-  area: number;
   population: number;
   flags: {
     png: string;
