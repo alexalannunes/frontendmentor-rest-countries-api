14ebb0eb22892ed168343518ce3f0b4833f67a87
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-07-07 18:45:52 -0300

    chore(mock): final mock

--- src/mock/country.ts
+++ src/mock/country.ts
@@ -14,4 +14,27 @@ export const countryMock: Country = {
     png: "https://flagcdn.com/w320/br.png",
     svg: "https://flagcdn.com/br.svg",
   },
+  currencies: {
+    BRL: {
+      name: "Brazilian real",
+      symbol: "R$",
+    },
+  },
+  languages: {
+    por: "Portuguese",
+  },
+  subRegion: "South America",
+  tld: [".br"],
+  borders: [
+    "ARG",
+    "BOL",
+    "COL",
+    "GUF",
+    "GUY",
+    "PRY",
+    "PER",
+    "SUR",
+    "URY",
+    "VEN",
+  ],
 };
