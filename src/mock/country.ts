59ede748dd5d3c0be67f8841b60dfb1af2777189
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-07-06 23:56:48 -0300

    chore(mock): add country mock

---
+++ src/mock/country.ts
@@ -0,0 +1,17 @@
+import { type Country } from "../types/country.type";
+
+export const countryMock: Country = {
+  name: {
+    common: "Brazil",
+  },
+  cca2: "BR",
+  ccn3: "076",
+  cca3: "BRA",
+  capital: ["Brasília"],
+  region: "Americas",
+  population: 212559409,
+  flags: {
+    png: "https://flagcdn.com/w320/br.png",
+    svg: "https://flagcdn.com/br.svg",
+  },
+};
